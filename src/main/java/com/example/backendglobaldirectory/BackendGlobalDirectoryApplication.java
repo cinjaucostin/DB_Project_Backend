604bbf93b1f9cc972ba77d4f24fe7caec400c942
fconstantin24 <fconstantin2405@stud.acs.upb.ro>
2023-06-09 14:48:22 +0300

    first commit

---
+++ src/main/java/com/example/backendglobaldirectory/BackendGlobalDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendglobaldirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGlobalDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGlobalDirectoryApplication.class, args);
+    }
+
+}
