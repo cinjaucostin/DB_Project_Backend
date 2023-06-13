a8a467d7354626d403f5aa27656b8d981d46eb1a
cinjaucostin <cinjau.costin@yahoo.com>
2023-06-13 13:46:04 +0300

    Authentication Controller and Service + User Controller, Service and Repository + Register DTO
    Signed-off-by: cinjaucostin <cinjau.costin@yahoo.com>

---
+++ src/main/java/com/example/backendglobaldirectory/dto/RegisterDTO.java
@@ -0,0 +1,4 @@
+package com.example.backendglobaldirectory.dto;
+
+public class RegisterDTO {
+}
---
+++ src/main/java/com/example/backendglobaldirectory/service/UserService.java
@@ -0,0 +1,4 @@
+package com.example.backendglobaldirectory.service;
+
+public class UserService {
+}
