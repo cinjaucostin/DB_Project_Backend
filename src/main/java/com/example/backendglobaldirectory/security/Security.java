002d7c17ea8779914a60d2f540c8fec1c8b3654b
Eduard-Costin <eduard_costin2001@yahoo.com>
2023-06-13 16:51:52 +0300

    login using formLogin
    Signed-off-by: Eduard-Costin <eduard_costin2001@yahoo.com>

---
+++ src/main/java/com/example/backendglobaldirectory/security/Security.java
@@ -0,0 +1,51 @@
+package com.example.backendglobaldirectory.security;
+
+import com.example.backendglobaldirectory.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Security {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().usernameParameter("email").permitAll()
+                .and()
+                .logout().permitAll()
+                .invalidateHttpSession(true);
+        return http.build();
+    }
+
+}
--- src/main/java/com/example/backendglobaldirectory/service/UserService.java
+++ src/main/java/com/example/backendglobaldirectory/service/UserService.java
@@ -1,4 +1,26 @@
 package com.example.backendglobaldirectory.service;
 
-public class UserService {
+import com.example.backendglobaldirectory.dto.MyUserDetails;
+import com.example.backendglobaldirectory.entities.User;
+import com.example.backendglobaldirectory.repository.UserRepository;
+import jakarta.persistence.Entity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = this.userRepository.getUserByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Not found!");
+        }
+        return new MyUserDetails(user);
+    }
 }
