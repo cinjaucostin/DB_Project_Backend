cebd93c52fedabc3cf9c0dfa145f5bff97cbc12d
fconstantin24 <fconstantin2405@stud.acs.upb.ro>
2023-06-23 13:50:29 +0300

    Implemented promotion post and update profile

--- src/main/java/com/example/backendglobaldirectory/service/EmailSenderService.java
+++ src/main/java/com/example/backendglobaldirectory/service/EmailSenderService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
